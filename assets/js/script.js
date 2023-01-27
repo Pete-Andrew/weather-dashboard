42d4e4808350bfb7f094221a884f69d95e23b6c9
Pete Andrew <112977421+Pete-Andrew@users.noreply.github.com>
2023-01-27 13:37:22 +0000

    5 day forcast working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,7 @@
-
-
 // get current time from moment
 var time = moment();
 // places the date in the main header 
 $("#momentDate").text(time.format("dddd, Do, MMMM"));
-
-// var city = $("#city"); 
-// city.text()
-
-// .text = $("#search-input").val();
-
-// Create and save a reference to new empty table row
-// var tableRow = $('<tr>')
-// // Create and save references to 3 td elements containing the Title, Year, and Actors from the AJAX response object
-// var tableDataTitle = $('<td id="title">')
-// var tableDataYear = $('<td id= "year">')
-// var tableDataActors = $('<td id="actors">')
-
 
 // Add your own API key between the ""
 var APIKey = "bd5a66ab99bbf1e26c28bc82ed4a9f87";
@@ -29,37 +14,124 @@ $("#search-button").on("click", function(event) {
 
   event.preventDefault();
 
-  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + APIKey;
+  //gets infomation for the location including lat and long which are needed for the 5 day forcast. 
+  var coordinateConverter = "http://api.openweathermap.org/geo/1.0/direct?q=" + location + "&appid=" + APIKey; 
+  // console.log(coordinateConverter);
 
-// We then created an AJAX call
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  // Create CODE HERE to Log the queryURL
-  console.log(queryURL);
-  // Create CODE HERE to log the resulting object
-  console.log(response);
+  $.ajax({
+    url: coordinateConverter,
+    method: "GET"
+  }).then(function(fiveDayResponse) {
+    
+    console.log(fiveDayResponse);
+
+    // creates lat and long vars from the response values from the coordinateConverter. 
+    var lat = fiveDayResponse[0].lat;
+    var long = fiveDayResponse[0].lon;
   
+
+    // uses lat and long to call a 5 day forcast from the API
+    var fiveDayForcast = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&appid=" + APIKey;
+    //converts this data into an array using ajax
+    $.ajax({
+      url: fiveDayForcast,
+      method: "GET"
+    }).then(function(fiveDayForcastResponse) {
+
+      console.log(fiveDayForcastResponse);
+
+      // var city = fiveDayForcastResponse.city.name;
+      var city = fiveDayResponse[0].name;
+      console.log(city);
+      $('#city').text(city);
+      
+      var humidity = fiveDayForcastResponse.list[0].main.humidity; 
+      $('#humidity').text("Relative humidity: " + humidity + "%");
+      console.log(humidity);
+      
+      var wind =  fiveDayForcastResponse.list[0].wind.speed; 
+      $('#wind').text("Wind speed: " + wind + " m/s");
+    
+      var KelvinTemp = fiveDayForcastResponse.list[0].main.temp;
+      var celciusTemp = KelvinTemp;
+      $("#temp").text(celciusTemp + " ºK");
+      
+
+      // var celsiusTemperature = Math.floor (KelvinTemp- 273.15);
+      // var myTempDiv = $("#temp");
+      // myTempDiv.text(celsiusTemperature + " ºC");
+    });
+    
+  }); 
+
+
+
+  // // Create CODE HERE to calculate the temperature (converted from Kelvin)
+  // var celsiusTemperature = Math.floor(fiveDayForcastResponse.list[0].main.temp - 273.15);
+  // // // Create CODE HERE to transfer content to HTML
+  // var myTempDiv = $("#temp");
+  // myTempDiv.text(celsiusTemperature + " ºC");
+  // var city = fiveDayForcastResponse[0].name;
+  // console.log(city);
+  // var humidity = response.main.humidity;
+  // var wind = response.wind.speed;
+  // // gets the index for the weather icon
+  // var queryWeatherIconCode = response.weather[0].icon;
+  // console.log(queryWeatherIconCode);
   
-  // Create CODE HERE to calculate the temperature (converted from Kelvin)
-  var celsiusTemperature = Math.floor(response.main.temp - 273.15);
-  // Create CODE HERE to transfer content to HTML
-  var myTempDiv = $("#temp");
-  myTempDiv.text(celsiusTemperature + " ºC");
-  var city = response.name;
-  var humidity = response.main.humidity;
-  var wind = response.wind.speed;
+  // var weatherIcon = $(queryWeatherIcon) + "@2x.png";
+  // <img src="http://openweathermap.org/img/w/${response.weather[0].icon}.png" alt="img"></img>
+
   $('#city').text(city);
+  // $("#weatherIcon").text(queryWeatherIcon);
   $('#humidity').text("Relative humidity: " + humidity + "%");
   $('#wind').text("Wind speed: " + wind + " m/s");
   // Hint: To convert from Kelvin to Celsius: C = K - 273.15
   // Create CODE HERE to dump the temperature content into HTML
+ 
+  
+  });
 
-});
 
-});
 
+// function weatherToday () {
+//   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + APIKey;
+//   // We then created an AJAX call
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   }).then(function(response) {
+//     // Create CODE HERE to Log the queryURL
+//     console.log(queryURL);
+//     // Create CODE HERE to log the resulting object
+//     console.log(response);
+    
+    
+//     // Create CODE HERE to calculate the temperature (converted from Kelvin)
+//     var celsiusTemperature = Math.floor(response.main.temp - 273.15);
+//     // Create CODE HERE to transfer content to HTML
+//     var myTempDiv = $("#temp");
+//     myTempDiv.text(celsiusTemperature + " ºC");
+//     var city = response.name;
+//     var humidity = response.main.humidity;
+//     var wind = response.wind.speed;
+//     // gets the index for the weather icon
+//     var queryWeatherIconCode = response.weather[0].icon;
+//     console.log(queryWeatherIconCode);
+    
+//     // var weatherIcon = $(queryWeatherIcon) + "@2x.png";
+//     // <img src="http://openweathermap.org/img/w/${response.weather[0].icon}.png" alt="img"></img>
+
+//     $('#city').text(city);
+//     // $("#weatherIcon").text(queryWeatherIcon);
+//     $('#humidity').text("Relative humidity: " + humidity + "%");
+//     $('#wind').text("Wind speed: " + wind + " m/s");
+//     // Hint: To convert from Kelvin to Celsius: C = K - 273.15
+//     // Create CODE HERE to dump the temperature content into HTML
+//   });
+// }
+
+// weatherToday(); 
 
 // api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid={}
 
