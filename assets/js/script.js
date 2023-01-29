db69d9f5b01fcd20b30171629e60d5c0c0b95e44
Pete Andrew <112977421+Pete-Andrew@users.noreply.github.com>
2023-01-29 21:12:53 +0000

    sepereated parts of code into functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,6 @@ $("#search-button").on("click", function(event) {
   if (location === "") {
     console.log("nope");
     return 0;
-
   }
   
   //gets infomation for the location including lat and long which are needed for the 5 day forcast.
@@ -71,52 +70,145 @@ $("#search-button").on("click", function(event) {
       var kelvinToCelciusConverter = 273.15;
       var KelvinTemp = fiveDayForcastResponse.list[0].main.temp;
       var celciusTemp = (KelvinTemp - kelvinToCelciusConverter).toFixed(2);
-      $("#temp").text(celciusTemp + " ºC");
+      $("#temp").text("temperature: " + celciusTemp + " ºC");
     });
   });
 
+  function createButtons () {
+  //creates new buttons on each location searched for
   var capitalButtonsDiv = document.getElementById("capitalsDiv");
 
   // creating button element
   var newButton = document.createElement("button");
-
+  //sets the attributes of the buttons to bootstrap configs
   newButton.setAttribute("class", "btn btn-secondary btn-block");
 
-  // creating text to be displayed on button
+  // creating text to be displayed on button (the place name entered)
   var newButtonText = document.createTextNode(location);
-  
-
-
   // appends text to button
   newButton.appendChild(newButtonText);
-
   // appending button to div
   capitalButtonsDiv.appendChild(newButton);
 
+  // How to save added buttons to local storage?? 
+  // on click save all of capitals div to local storage?? 
+  
+}
 
-  // $('#newButton').addClass('btn btn-secondary btn-block');
+createButtons(); 
 });
 
 
-  // var myDiv = document.getElementById("capitalsDiv");
-                 
-  //               // creating button element
-  //               var button = document.createElement('BUTTON');
-                 
-  //               // creating text to be
-  //               //displayed on button
-  //               var text = document.createTextNode("Button");
-                 
-  //               // appending text to button
-  //               button.appendChild(text);
-                 
-  //               // appending button to div
-  //               myDiv.appendChild(button); ;
+//saving the city buttons using local storage
+//need to create an array to hold city buttons data 
+function saveToLocalStorage (newButtonsText) {
 
-// var newButtonsDiv = $("capitalsDiv");
-// document.createElement("button"); 
-// var newButtonText = location;
-// newButtonsDiv.appendChild("button");
+var citiesEntered = [];
+
+for (var i = 0; i < citiesEntered.length; i++) {
+  var citiesEnteredArray = citiesEntered[i]; 
+}
+
+citiesEntered.push(newButtonsText);
+
+
+localStorage.setItem("citiesEntered", JSON.stringify(citiesEntered));
+
+console.log(citiesEnteredArray);
+
+}
+
+saveToLocalStorage (); 
+
+
+
+var todoInput = document.querySelector("#todo-text");
+var todoForm = document.querySelector("#todo-form");
+var todoList = document.querySelector("#todo-list");
+var todoCountSpan = document.querySelector("#todo-count");
+
+var todos = [];
+
+init();
+
+function renderTodos() {
+  // Clear todoList element and update todoCountSpan
+  todoList.innerHTML = "";
+  todoCountSpan.textContent = todos.length;
+
+  // Render a new li for each todo
+  for (var i = 0; i < todos.length; i++) {
+    var todo = todos[i];
+
+    var li = document.createElement("li");
+    li.textContent = todo;
+    li.setAttribute("data-index", i);
+
+    var button = document.createElement("button");
+    button.textContent = "Complete";
+
+    li.appendChild(button);    //<li data-index="1">LearnCSS <button>Complete</button></li>
+    todoList.appendChild(li);
+  }
+}
+
+function init() {
+  // Get stored todos from localStorage
+  // Parsing the JSON string to an object
+  var storedTodos = JSON.parse(localStorage.getItem("todos"));
+
+  // If todos were retrieved from localStorage, update the todos array to it
+  if (storedTodos !== null) {
+    todos = storedTodos;
+  }
+
+  // Render todos to the DOM
+  renderTodos();
+}
+
+function storeTodos() {
+  // Stringify and set "todos" key in localStorage to todos array
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+// When form is submitted...
+todoForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  var todoText = todoInput.value.trim();
+
+  // Return from function early if submitted todoText is blank
+  if (todoText === "") {
+    return;
+  }
+
+  // Add new todoText to todos array, clear the input
+  todos.push(todoText);
+  todoInput.value = "";
+
+  // Store updated todos in localStorage, re-render the list
+  storeTodos();
+  renderTodos();
+});
+
+// When a element inside of the todoList is clicked...
+todoList.addEventListener("click", function(event) {
+  var element = event.target;
+
+  // If that element is a button...
+  if (element.matches("button") === true) {
+    // Get its data-index value and remove the todo element from the list
+    var index = element.parentElement.getAttribute("data-index");
+    todos.splice(index, 1);
+
+    // Store updated todos in localStorage, re-render the list
+    storeTodos();
+    renderTodos();
+  }
+});
+
+
+
 
 // api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid={}
 
